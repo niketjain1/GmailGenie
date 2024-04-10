211135893cef865b6465e4247e11f958b748db29
niketjain1 <niketj2000@gmail.com>
2024-04-10 20:12:02 +0530

    📦 NEW: added scope for reading and writing emails

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -9,7 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:3000/auth/google/callback',
-      scope: ['email', 'profile'],
+      scope: ['email', 'profile', 'https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send'],
     });
   }
 
