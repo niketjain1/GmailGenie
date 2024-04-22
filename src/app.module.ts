d98b0d56566ec6072aedc7a7ebe88b397c421900
niketjain1 <niketj2000@gmail.com>
2024-04-22 13:08:52 +0530

    Configure the app module file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { QueueService } from './queue/queue.service';
+import { BullModule } from '@nestjs/bull';
+import { QueueConsumer } from './queue/queue.consumer';
+import { GmailHelper } from './gmail/gmail.helper';
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({
-    envFilePath: ['.env.local'],
-  })],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local'],
+    }),
+    BullModule.forRoot({
+      redis: {
+        host: '0.0.0.0',
+        port: 6379,
+      },
+    }),
+    BullModule.registerQueue({
+      name: 'processEmailsQueue',
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, QueueConsumer, QueueService, GmailHelper],
 })
 export class AppModule {}
